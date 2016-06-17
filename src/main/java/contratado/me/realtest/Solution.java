1970365b2ee154b9443a482669c94644e8e8d2a0
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-06-17 08:30:08 -0300

    More testing

---
+++ src/main/java/contratado/me/realtest/Solution.java
@@ -0,0 +1,23 @@
+package contratado.me.realtest;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        String s = scanner.next();
+
+    }
+
+
+}
